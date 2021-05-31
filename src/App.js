6f7f225012ff36a81520f2a91084b291cba8f435
Sshivani-12 <shivaniwakde1211@gmail.com>
2021-05-31 13:16:34 +0530

    added cart files

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
 import Details from "./components/Details";
-import Default from "./components/Default";
+import Default from "./components/Cart/Default";
 import Cart from "./components/Cart/Cart";
 import Modal from "./components/Modal"
 
