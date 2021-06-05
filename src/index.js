4f1c168b2ce0f61fe4fe4676610dc28d1a0a5356
Sshivani-12 <shivaniwakde1211@gmail.com>
2021-06-05 18:51:10 +0530

    added new features

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {ProductProvider} from './context';
-import Product from './components/Product';
 
 ReactDOM.render(
   <ProductProvider>
